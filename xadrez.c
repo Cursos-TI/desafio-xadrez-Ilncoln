423455cfb720148dfb83712bd2645199d0f9b433
Ilncoln <ismaellincoln02@outlook.com>
2025-07-24 12:58:49 +0000

    Desafio: Nivel Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -16,17 +16,35 @@ int main() {
     printf("\nBispo se move para: \n");
 
     while (bispo < 5)
-    {printf("- Cima, Direita ");
+    {printf(" - Cima");
+     printf(" - Direita");
      bispo++;   
     }
 
     //Movimento da rainha
-    printf("\nRainha se move para: \n");
+    printf("\nRainha se move pra: \n");
 
     do
     {printf("- Esquerda ");
       rainha++;  
     } while (rainha < 8);
+
+    //Movimento do cavalo
+    printf("\nCavalo salta para: \n");
+
+    int cavalo, movimento = 0;
+
+    for(cavalo = 0; cavalo == movimento; cavalo++){
+
+        while (movimento < 2)
+        {
+            printf("Baixo\n");
+            movimento++;
+        }
+        printf("Esquerda\n");
+    }
+
+
     
     
     return 0;
